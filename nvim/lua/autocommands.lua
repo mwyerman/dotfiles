5dd53ed82f534d2b47685935adc5d591d687c3ff
Max Wyerman <mwyerman@gmail.com>
2023-01-21 12:39:12 -0500

    Add autocommand to change commentstring (#2)

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -6,26 +6,41 @@ vim.cmd [[
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
   augroup end
+]]
 
+vim.cmd [[
   augroup _git
     autocmd!
     autocmd FileType gitcommit setlocal wrap
     autocmd FileType gitcommit setlocal spell
   augroup end
+]]
 
+vim.cmd [[
   augroup _markdown
     autocmd!
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
   augroup end
+]]
 
+vim.cmd [[
   augroup _auto_resize
     autocmd!
     autocmd VimResized * tabdo wincmd = 
   augroup end
+]]
 
+vim.cmd [[
   augroup _alpha
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+]]
+
+-- change commentstring to use // for c, h, cpp, and hpp files
+vim.cmd [[
+  augroup _commentstring
+    autocmd!
+    autocmd BufEnter *.{c,h,cpp,hpp} setlocal commentstring=//\ %s
 ]]
