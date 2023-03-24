8eb2f5123100d9e01c5ebfe4dfc6951d1391d679
Max Wyerman <mwyerman@gmail.com>
2023-03-24 10:14:44 -0400

    Change git graph alias

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -65,7 +65,8 @@ function gg() {
   else
     NUMOPT="-n $1"
   fi
-  git log --oneline --decorate --graph --all --color --pretty=format:'%Cred%h%Creset - %Cgreen%ah%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an|%ae>%Creset' $NUMOPT
+  # git log --oneline --decorate --graph --all --color --pretty=format:'%Cred%h%Creset - %Cgreen%ah%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an|%ae>%Creset' $NUMOPT
+  git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)' $NUMOPT
 }
 
 
