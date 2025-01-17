1aae5b5965e8666d25fd7a4efc4a5c2c8dcef4fd
Max Wyerman <mwyerman@gmail.com>
2025-01-17 09:42:42 -0500

    Add fish git abbreviations

---
+++ dot_config/private_fish/conf.d/abbreviations.fish
@@ -0,0 +1,14 @@
+# git
+abbr --add gs 'git status -s'
+abbr --add gS 'git status'
+abbr --add ga 'git add'
+abbr --add gA 'git add .'
+abbr --add gcm 'git commit -m'
+abbr --add gca 'git commit --amend'
+abbr --add gcA 'git commit --amend --no-edit'
+abbr --add gd 'git diff'
+abbr --add gp 'git push'
+abbr --add gpl 'git pull'
+abbr --add gf 'git fetch'
+abbr --add gl 'git log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short'
+
