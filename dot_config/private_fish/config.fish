9bc34e98b66ef5ba23333b647138dc525fa991a1
Maxwell Wyerman <mwyerman@gmail.com>
2025-01-19 12:00:51 -0500

    Resolve merge conflict

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,5 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Created by `pipx` on 2025-01-14 16:30:32
-set PATH $PATH /home/mwyerman/.local/bin
+set PATH $PATH ~/.local/bin
