173e1fa502535387450a6fa8b920ded70e98f38d
Maxwell Wyerman <mwyerman@gmail.com>
2025-01-25 12:20:37 -0500

    Update nvim autocomplete and lsp

--- dot_config/nvim/lua/mwyerman/plugins/lsp.lua
+++ dot_config/nvim/lua/mwyerman/plugins/lsp.lua
@@ -26,172 +26,138 @@ local function create_lsp_keymaps(event)
     -- stylua: ignore end
 end
 
+local ENSURE_INSTALLED = {
+    "lua_ls",
+    "rust_analyzer",
+    "pyright",
+    "clangd",
+    "cssls",
+    "eslint",
+    "jsonls",
+    "ts_ls",
+    "taplo",
+    "html",
+    "yamlls",
+    "cmake",
+    "dockerls",
+    "bashls",
+    "powershell_es",
+    "stylua",
+    "prettier",
+    "clang-format",
+}
+
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/nvim-cmp",
-        "j-hui/fidget.nvim",
-    },
-
-    config = function()
-        --- attach keymaps
-        vim.api.nvim_create_autocmd("LspAttach", {
-            desc = "LSP Keymaps",
-            callback = function(args)
-                create_lsp_keymaps(args)
-
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if
-                    client.server_capabilities.inlayHintProvider
-                    and not vim.lsp.inlay_hint.is_enabled()
-                then
-                    vim.lsp.inlay_hint.enable(true)
-                end
-            end,
-        })
-
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
-
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "pyright",
-                "clangd",
-                "cssls",
-                "eslint",
-                "jsonls",
-                "ts_ls",
-                "taplo",
-                "html",
-                "yamlls",
-                "cmake",
-                "dockerls",
-                "bashls",
-                "powershell_es",
-                "stylua",
-                "prettier",
-                "clang-format",
+    { -- nvim autocompletion for lsp
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
             },
-        })
-        require("mason-lspconfig").setup({
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "j-hui/fidget.nvim",
+            "saghen/blink.cmp",
+        },
+
+        config = function()
+            --- attach keymaps
+            vim.api.nvim_create_autocmd("LspAttach", {
+                desc = "LSP Keymaps",
+                callback = function(args)
+                    create_lsp_keymaps(args)
+
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if
+                        client ~= nil
+                        and client.server_capabilities.inlayHintProvider
+                        and not vim.lsp.inlay_hint.is_enabled()
+                    then
+                        vim.lsp.inlay_hint.enable(true)
+                    end
                 end,
-                rust_analyzer = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.rust_analyzer.setup({
-                        settings = {
-                            ["rust-analyzer"] = {
-                                cargo = {
-                                    features = "all",
-                                },
-                                inlayHints = {
-                                    enable = true,
-                                },
-                            },
-                        },
-                    })
-                end,
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(
-                            ".git",
-                            "build.zig",
-                            "zls.json"
-                        ),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                end,
-                lua_ls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = {
-                                        "bit",
-                                        "vim",
-                                        "it",
-                                        "describe",
-                                        "before_each",
-                                        "after_each",
+            })
+
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+            require("fidget").setup({})
+            require("mason").setup()
+            require("mason-tool-installer").setup({
+                ensure_installed = ENSURE_INSTALLED,
+            })
+            require("mason-lspconfig").setup({
+                ensure_installed = {},
+                automatic_installation = false,
+                handlers = {
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                    rust_analyzer = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.rust_analyzer.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                ["rust-analyzer"] = {
+                                    cargo = {
+                                        features = "all",
+                                    },
+                                    inlayHints = {
+                                        enable = true,
                                     },
                                 },
                             },
-                        },
-                    })
-                end,
-            },
-        })
+                        })
+                    end,
+                },
+            })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        for _, method in ipairs({
-            "textDocument/diagnostic",
-            "workspace/diagnostic",
-        }) do
-            local default_diagnostic_handler = vim.lsp.handlers[method]
-            vim.lsp.handlers[method] = function(err, result, context, config)
-                if err ~= nil and err.code == -32802 then
-                    return
+            for _, method in ipairs({
+                "textDocument/diagnostic",
+                "workspace/diagnostic",
+            }) do
+                local default_diagnostic_handler = vim.lsp.handlers[method]
+                vim.lsp.handlers[method] = function(
+                    err,
+                    result,
+                    context,
+                    config
+                )
+                    if err ~= nil and err.code == -32802 then
+                        return
+                    end
+                    return default_diagnostic_handler(
+                        err,
+                        result,
+                        context,
+                        config
+                    )
                 end
-                return default_diagnostic_handler(err, result, context, config)
             end
-        end
 
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<cr>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "path" },
-            }, {
-                { name = "buffer" },
-            }),
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end,
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = true,
+                    header = "",
+                    prefix = "",
+                },
+            })
+        end,
+    },
 }
